b573c9eb88eaae7aae4d3be16af334020b564a33
MRJN <marijan@necoski.de>
2025-04-28 20:18:38 +0200

    builddate

--- smart-build.sh
+++ smart-build.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+set -e
+
 BASE_IMAGE="debian:bookworm-slim"
 DIGEST_FILE=".last_base_digest"
-docker pull $BASE_IMAGE
-CURRENT_DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' $BASE_IMAGE)
+
+docker pull "$BASE_IMAGE"
+CURRENT_DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' "$BASE_IMAGE")
 
 if [ -f "$DIGEST_FILE" ]; then
     LAST_DIGEST=$(cat "$DIGEST_FILE")
     if [ "$CURRENT_DIGEST" == "$LAST_DIGEST" ]; then
-        echo "Image not changed."
+        echo "Base image unchanged, skipping build."
         exit 0
     fi
 fi
+
 echo "$CURRENT_DIGEST" > "$DIGEST_FILE"
+
+DATE_TAG=$(date +'%Y%m%d%H%M')
+
 docker build -t apt-cacher-ng .
+
+docker tag apt-cacher-ng ghcr.io/mrjncsk/apt-cacher-ng:${DATE_TAG}
+docker push ghcr.io/mrjncsk/apt-cacher-ng:${DATE_TAG}
+
+docker tag apt-cacher-ng ghcr.io/mrjncsk/apt-cacher-ng:latest
+docker push ghcr.io/mrjncsk/apt-cacher-ng:latest
